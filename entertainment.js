06bdade28e0caca2de4adb9ef5ce87d84053aaa3
maixxrah617 <maixxrah617@gmail.com>
2025-12-10 01:13:32 +0000

    Add event listeners

--- entertainment.js
+++ entertainment.js
@@ -1,1 +1,15 @@
+//Selects all images.
+const images = document.querySelectorAll('.entertainment-img');
 
+//Loops through each image
+images.forEach(img => {
+  img.addEventListener('mouseenter', () => { //When mouse hovers over, the image grows.
+    img.style.transition = 'transform 0.3s ease'; //For smooth animation.
+    img.style.transform = 'scale(1.2)';
+  });
+
+//When mouse leaves, the image shrinks.
+  img.addEventListener('mouseleave', () => {
+    img.style.transform = 'scale(1)';
+  });
+});
